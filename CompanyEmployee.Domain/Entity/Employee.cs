3eb0932a73e34f7df9258fa5d761700d962a78b6
Андрей <andreyka_kazakov_03@mail.ru>
2026-02-24 20:23:56 +0400

    Начало?
    Стыдно должно быть, но пока рано

---
+++ CompanyEmployee.Domain/Entity/Employee.cs
@@ -0,0 +1,63 @@
+namespace CompanyEmployee.Domain.Entities;
+
+public class Employee
+{
+    /// <summary>
+    /// Идентификатор сотрудника в системе
+    /// </summary>
+    public int Id { get; set; }
+
+
+    /// <summary>
+    /// ФИО
+    /// </summary>
+    public string FullName { get; set; } = string.Empty;
+
+
+    /// <summary>
+    /// Должность
+    /// </summary>
+    public string Position { get; set; } = string.Empty;
+
+
+    /// <summary>
+    /// Отдел
+    /// </summary>
+    public string Department { get; set; } = string.Empty;
+
+
+    /// <summary>
+    /// Дата приема
+    /// </summary>
+    public DateOnly HireDate { get; set; }
+
+
+    /// <summary>
+    /// Зарплата
+    /// </summary>
+    public decimal Salary { get; set; }
+
+
+    /// <summary>
+    /// Электронная почта
+    /// </summary>
+    public string Email { get; set; } = string.Empty;
+
+
+    /// <summary>
+    /// Телефон
+    /// </summary>
+    public string Phone { get; set; } = string.Empty;
+
+
+    /// <summary>
+    /// Индикатор увольнения
+    /// </summary>
+    public bool IsTerminated { get; set; }
+
+
+    /// <summary>
+    /// Дата увольнения
+    /// </summary>
+    public DateOnly? TerminationDate { get; set; }
+}
