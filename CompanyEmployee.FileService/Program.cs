ad91d6a695ec6e893a89ab6038c588c86a9c23ac
Андрей <andreyka_kazakov_03@mail.ru>
2026-04-03 19:59:07 +0400

    В процессе

--- CompanyEmployee.FileService/Program.cs
+++ CompanyEmployee.FileService/Program.cs
@@ -1,6 +1,19 @@
+using CompanyEmployee.FileService.Services;
+using CompanyEmployee.ServiceDefaults;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.AddServiceDefaults();
+
+builder.Services.Configure<SnsSettings>(builder.Configuration.GetSection("SNS"));
+builder.Services.Configure<StorageSettings>(builder.Configuration.GetSection("Storage"));
+
+builder.Services.AddSingleton<IStorageService, MinioStorageService>();
+builder.Services.AddHostedService<SnsConsumerService>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapDefaultEndpoints();
+app.MapGet("/health", () => "Healthy");
 
 app.Run();
